8c888957ec3cb73140004c9b490105d7d809ff67
lanyuan <2568754256@qq.com>
2022-11-18 22:26:17 +0800

    1.新增chapter页面及子路由，并使用假数据制作表格

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router"
 import Login from "./views/login.vue"
 import Admin from "./views/admin.vue"
 import Welcome from "./views/admin/welcome.vue"
+import Chapter from "./views/admin/chapter.vue";
 // import Category from "./views/admin/category.vue"
 // import Course from "./views/admin/course.vue"
 // import Chapter from "./views/admin/chapter.vue"
@@ -30,64 +31,14 @@ export default new Router({
     }, {
         path: "/admin",
         component: Admin,
-        // meta: {
-        //     loginRequire: true
-        // }
-        //     },
         /*子路由不要加斜杠*/
         children: [{
             path: "welcome",
             name: "welcome",
             component: Welcome
+        }, {
+            path: "chapter",
+            component: Chapter,
         }]
-        //     }, {
-        //         path: "business/category",
-        //         name: "business/category",
-        //         component: Category,
-        //     }, {
-        //         path: "business/course",
-        //         name: "business/course",
-        //         component: Course,
-        //     }, {
-        //         path: "business/chapter",
-        //         name: "business/chapter",
-        //         component: Chapter,
-        //     }, {
-        //         path: "business/section",
-        //         name: "business/section",
-        //         component: Section,
-        //     }, {
-        //         path: "business/content",
-        //         name: "business/content",
-        //         component: Content,
-        //     }, {
-        //         path: "business/teacher",
-        //         name: "business/teacher",
-        //         component: Teacher,
-        //     }, {
-        //         path: "business/member",
-        //         name: "business/member",
-        //         component: Member,
-        //     }, {
-        //         path: "business/sms",
-        //         name: "business/sms",
-        //         component: Sms,
-        //     }, {
-        //         path: "file/file",
-        //         name: "file/file",
-        //         component: File,
-        //     }, {
-        //         path: "system/user",
-        //         name: "system/user",
-        //         component: User,
-        //     }, {
-        //         path: "system/resource",
-        //         name: "system/resource",
-        //         component: Resource,
-        //     }, {
-        //         path: "system/role",
-        //         name: "system/role",
-        //         component: Role,
-        //     }]
     }]
 })
