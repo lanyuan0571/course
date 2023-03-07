8d6a611074bf20f1c1d7336eda4a17b071593312
lanyuan <2568754256@qq.com>
2023-03-07 17:06:20 +0800

    1.用户管理与登录：解决从登录页面跳到控台主页时，侧边栏失效的问题

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -519,6 +519,8 @@ export default {
     // console.log("admin");
     // sidebar激活样式方法二
     _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+    /*重新加载ACE.js*/
+    $.getScript('/ace/assets/js/ace.min.js');
   },
   watch: {
     $route: {
