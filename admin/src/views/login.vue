903d7f98c1a7173bd743d68aee8d7788d2691be0
lanyuan <2568754256@qq.com>
2023-03-08 15:37:52 +0800

    1.用户管理与登录：增加记住登录信息功能

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -7,10 +7,8 @@
             <div class="center">
               <h1>
                 <i class="ace-icon fa fa-leaf green"></i>
-                <span class="red">Ace</span>
-                <span class="white" id="id-text2">Application</span>
+                <span class="">控台登录</span>
               </h1>
-              <h4 class="blue" id="id-company-text">&copy; Company Name</h4>
             </div>
 
             <div class="space-6"></div>
@@ -21,66 +19,86 @@
                   <div class="widget-main">
                     <h4 class="header blue lighter bigger">
                       <i class="ace-icon fa fa-coffee green"></i>
-                      请输入用户名和密码
+                      请输入用户名密码
                     </h4>
+
                     <div class="space-6"></div>
+
                     <form>
                       <fieldset>
                         <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input v-model="user.loginName" type="text" class="form-control" placeholder="用户名"/>
-															<i class="ace-icon fa fa-user"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input v-model="user.loginName" type="text" class="form-control" placeholder="用户名"/>
+                            <i class="ace-icon fa fa-user"></i>
+                          </span>
                         </label>
+
                         <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input v-model="user.password" type="password" class="form-control" placeholder="密码"/>
-															<i class="ace-icon fa fa-lock"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input v-model="user.password" type="password" class="form-control" placeholder="密码"/>
+                            <i class="ace-icon fa fa-lock"></i>
+                          </span>
                         </label>
+
                         <div class="space"></div>
+
                         <div class="clearfix">
                           <label class="inline">
-                            <input type="checkbox" class="ace"/>
-                            <span class="lbl">记住密码</span>
+                            <input v-model="remember" type="checkbox" class="ace"/>
+                            <span class="lbl">记住我</span>
                           </label>
 
                           <button type="button"
-                                  @click="login()"
-                                  class="width-35 pull-right btn btn-sm btn-primary">
+                                  class="width-35 pull-right btn btn-sm btn-primary"
+                                  v-on:click="login()">
                             <i class="ace-icon fa fa-key"></i>
-                            <span class="bigger-110">Login</span>
+                            <span class="bigger-110">登录</span>
                           </button>
                         </div>
+
                         <div class="space-4"></div>
                       </fieldset>
                     </form>
+
                   </div><!-- /.widget-main -->
+
                 </div><!-- /.widget-body -->
               </div><!-- /.login-box -->
             </div><!-- /.position-relative -->
+
           </div>
         </div><!-- /.col -->
       </div><!-- /.row -->
     </div><!-- /.main-content -->
   </div><!-- /.main-container -->
 </template>
+
 <script>
 export default {
-  name: 'login',
+  name: "login",
   data: function() {
     return {
       user: {},
+      remember: false // 默认不勾选记住我
     }
   },
-  mounted: function () {
-    $('body').removeClass('no-skin')
-    $('body').attr('class', 'login-layout light-login');
+  mounted: function() {
+    let _this = this;
+    $("body").removeClass("no-skin");
+    $("body").attr("class", "login-layout light-login");
+    // console.log("login");
+
+    // 从缓存中获取记住的用户名密码，如果获取不到，说明上一次没有勾选“记住我”
+    let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER);
+    if (rememberUser) {
+      _this.user = rememberUser;
+    }
   },
   methods: {
     login () {
       let _this = this;
 
+      let passwordShow = _this.user.password;
       _this.user.password = hex_md5(_this.user.password + KEY);
       Loading.show();
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response)=>{
@@ -88,7 +106,20 @@ export default {
         let resp = response.data;
         if (resp.success) {
           console.log("登录成功：", resp.content);
+          let loginUser = resp.content;
           Tool.setLoginUser(resp.content);
+
+          // 判断“记住我”
+          if (_this.remember) {
+            // 如果勾选记住我，则将用户名密码保存到本地缓存，这里需要保存密码明文，否则登录时又会再加一层密
+            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, {
+              loginName: loginUser.loginName,
+              password: passwordShow
+            });
+          } else {
+            // 没有勾选“记住我”时，要把本地缓存清空，否则按照mounted的逻辑，下次打开时会自动显示用户名密码
+            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, null);
+          }
           _this.$router.push("/welcome")
         } else {
           Toast.warning(resp.message)
