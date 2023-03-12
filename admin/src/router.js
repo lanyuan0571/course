cc42a44a9a3011b99d2a086a761bbe47e712a273
lanyuan <2568754256@qq.com>
2023-03-12 15:48:26 +0800

    1.会员登录注册功能开发，增加短信管理，生成代码后，需要配置资源权限

--- admin/src/router.js
+++ admin/src/router.js
@@ -15,7 +15,7 @@ import User from "./views/admin/user.vue"
 import Resource from "./views/admin/resource.vue"
 import Role from "./views/admin/role.vue"
 import Member from "./views/admin/member.vue"
-// import Sms from "./views/admin/sms.vue"
+import Sms from "./views/admin/sms.vue"
 
 Vue.use(Router);
 
@@ -71,6 +71,10 @@ export default new Router({
             path: "business/member",
             name: "business/member",
             component: Member,
+        }, {
+            path: "business/sms",
+            name: "business/sms",
+            component: Sms,
         }, {
             path: "file/file",
             name: "file/file",
