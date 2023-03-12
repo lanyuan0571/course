15bcebcba1333bcf48729114a18368bdaa5dba6a
lanyuan <2568754256@qq.com>
2023-03-12 15:17:26 +0800

    1.会员登录注册功能开发，登录成功后，头部导航显示会员昵称

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -5,8 +5,7 @@
         <a class="navbar-brand" href="#">
           <i class="ace-icon fa fa-video-camera"></i>&nbsp;Hedwig课程
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -19,8 +18,7 @@
               <router-link class="nav-link" to="/list">全部课程</router-link>
             </li>
             <li class="nav-item dropdown active">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 更多
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -31,9 +29,8 @@
               </div>
             </li>
           </ul>
-          <span class="text-white">欢迎：</span>
-          <button v-on:click="openLoginModal()" class="btn btn-outline-light my-2 my-sm-0" type="submit">登录/注册
-          </button>
+          <span v-show="loginMember.id" class="text-white pr-3">您好：{{loginMember.name}}</span>
+          <button v-on:click="openLoginModal()" class="btn btn-outline-light my-2 my-sm-0" type="submit">登录/注册</button>
         </div>
       </div>
     </nav>
@@ -45,10 +42,18 @@
 <script>
 
 import TheLogin from "./login";
-
 export default {
   name: 'theHeader',
   components: {TheLogin},
+  data: function () {
+    return {
+      loginMember: {}
+    }
+  },
+  mounted() {
+    let _this = this;
+    _this.loginMember = Tool.getLoginMember();
+  },
   methods: {
     /**
      * 打开登录注册窗口
@@ -56,6 +61,11 @@ export default {
     openLoginModal() {
       let _this = this;
       _this.$refs.loginComponent.openLoginModal();
+    },
+
+    setLoginMember(loginMember) {
+      let _this = this;
+      _this.loginMember = loginMember;
     },
 
   }
